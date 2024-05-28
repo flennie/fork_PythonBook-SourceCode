3532172b5481cefb3272c1c86e50655a182a6483
Lennert <ljdenhollander@gmail.com>
2024-05-29 00:15:03 +0200

    Create oefenopgave 1.py

---
+++ Own work/oefenopgave 1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 29 00:04:17 2024
+
+@author: lennert
+"""
+
+a = 100 -99;
+b = '1' + 'a';
+c=1+a;
+d=b+b;
+
+
+e = 12+6+3; # int
+f='12'+'6'+'3' ;# str
+g="7"+'7'; # str
+h='8';
+i=h+h+h; #str
+
+# 4 uit 5
+vervoermiddel = "auto"
+vervoermiddel = vervoermiddel + "mobiel"
+print (vervoermiddel)
+
+telefoonnummer = '0987654321'
+print ("Mijn telefoonnummer is " + telefoonnummer)
+
+leeftijd = 46
+print ("Mijn leeftijd is " + str(leeftijd))
+
+# 5 uit 5
+x, z, y = 5, 4, 3
+
+print (x + y)
+
